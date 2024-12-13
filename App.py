f36727f852556877c8156eda42c014254b0f0283
Your Name <youremail@example.com>
2024-12-13 21:37:44 +0700

    Initial commit of multipage Streamlit app

--- App.py
+++ App.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
+st.image("C:\Users\LENOVO\Documents\ImageProcessingApp - Group 8 - Add Logo and Photo\style\logo pu.png", width=150)
+
 st.title("Welcome Image Processing Web Application")
-# Display the logo
-st.image("C:\Users\LENOVO\Documents\ImageProcessingApp - Group 8\style\logo pu.png", width=150)
 
 st.sidebar.title("Navigation")
 st.sidebar.write("Use the menu to navigate.")
