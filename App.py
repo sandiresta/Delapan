a4876f94c62c12b7ae5886aa177bd51287413332
sandiresta <atseridnas23@gmail.com>
2024-12-13 21:14:53 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,8 +1,5 @@
 import streamlit as st
 
-# Display the logo
-st.image("C:\Users\LENOVO\Documents\ImageProcessingApp - Group 8\style\logo pu.png", width=150)
-
 st.title("Welcome to Image Processing Web Application DELAPAN")
 
 st.sidebar.title("Navigation")
